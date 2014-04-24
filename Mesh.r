ea036f610d14f32b5651e6b24c703a5570bf2874
JVAdams <buai.biz@gmail.com>
2014-04-24 08:54:50 -0500

    mesh slider

--- Mesh.r
+++ Mesh.r
@@ -5,24 +5,26 @@
 #############################  input values  #############################
 ##########################################################################
 
-# the extent to which the mesh is stretched while fishing, expressed as height as a percentage of width
-pctopen <- 0.35
+# the extent to which the mesh is stretched while fishing, expressed as height / width
+pctopen <- 0.2
 
-# mesh in inches
-mesh <- 2.5
+# bar mesh in inches (stretch mesh = bar mesh * 2)
+barmesh <- 1.5
 
 # fish length to body depth ratio
-ldratio <- 8
+ldratio <- 4.8
 
-# fish body depth
-bodydepth <- 1.25
+# fish body depth in inches
+bodydepth <- 1
 
 ##########################################################################
 ############################  data crunching  ############################
 ##########################################################################
 
-# load the plotrix package
+# load the plotrix, MASS, and rpanel packages
 if(!require(plotrix)) stop("R package plotrix is required")
+if(!require(MASS)) stop("R package MASS is required")
+if(!require(rpanel)) stop("R package rpanel is required")
 
 polar2cart <- function(r, theta, degrees=FALSE) {
 	# http://stackoverflow.com/questions/16351178/r-converting-cartesian-coordinates-to-polar-coordinates-and-then-calculating-d
@@ -38,15 +40,23 @@ polar2cart <- function(r, theta, degrees=FALSE) {
 	c(x=x, y=y)
 	}
 
-length <- bodydepth*ldratio*25.4
+strmesh <- 2 * barmesh
 
-xy <- polar2cart(mesh, asin(mesh/mesh*pctopen))
-x <- xy["x"]
-y <- xy["y"]
+# mesh and ellipse graphing function
+drawmesh <- function(panel) {
+	flength <- round(panel$bodydepth*panel$ldratio*25.4)
+	L <- sqrt(4*panel$barmesh^2 / (panel$pctopen^2 + 1))
+	H <- L * panel$pctopen
+	eqscplot(c(0, L, 0, -L, 0)/2, c(-H, 0, H, 0, -H)/2, type="l", las=1, xlab="Inches", ylab="Inches", 
+		main=paste0('mesh opening = ', 100*panel$pctopen, '%, bar mesh = ', panel$barmesh, 
+		'",\nfish len/dpth ratio = ', panel$ldratio, ', body dpth = ', panel$bodydepth, '", len = ', flength, ' mm'), lwd=2)
+	draw.ellipse(x=0, y=0, a=panel$bodydepth/2, b=panel$bodydepth/4, angle=0, col="gray", lwd=2)
+	panel
+	}
 
+# plot it, with a slider to adjust coefficients of the double logistic function
 windows()
 par(mar=c(3, 3, 3, 1))
-eqscplot(c(0, y, 0, -y, 0), c(-x, 0, x, 0, -x), type="l", las=1, xlab="", ylab="", 
-	main=paste0('pctopen = ', 100*pctopen, '%, mesh = ', mesh, 
-	'",\nldratio = ', ldratio, ', bodydepth = ', bodydepth, '", length = ', length, ' mm'), lwd=2)
-draw.ellipse(x=0, y=0, a=bodydepth, b=bodydepth/2, angle=0, col="gray", lwd=2)
+panel <- rp.control(bodydepth=bodydepth, ldratio=ldratio, barmesh=barmesh, pctopen=pctopen)
+rp.slider(panel, bodydepth, 0.01, 5, resolution=0.01, showvalue=T, action=drawmesh, title="Body depth (inches)")
+rp.slider(panel, ldratio, 1, 20, resolution=0.1, showvalue=T, action=drawmesh, title="Fish length to depth ratio")
